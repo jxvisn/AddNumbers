625fb52d88fd35a6dfff4bac23dc260fe8340536
jxvisn <jxvisn@gmail.com>
2019-12-12 04:46:24 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //We need a loop and ask a user to enter a number
+            //We need a place to store all the numbers the user  gives
+            //Calculate and add up all the numbers
+            //Display sum on Console
+
+        }
+    }
+}
